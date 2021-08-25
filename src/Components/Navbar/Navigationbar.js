c530155526f41608d85c1145d4fd861c2c8cea5b
rose koning <rose@roses-MacBook-Air.local>
2021-08-25 17:38:01 +0200

    add working navbar

---
+++ src/Components/Navbar/Navigationbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Link} from "react-router-dom"
+import { Navbar,Nav,NavDropdown,Container } from 'react-bootstrap';
+import Home from '../../Pages/Home';
+import About from '../../Pages/About';
+
+export default function Navigationbar(){
+    return(
+        <div>
+           <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">Horse Tracks</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="/home">Home</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="/about">Contact</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+        </div>
+    )
+}
